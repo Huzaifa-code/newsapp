fc006caf7a2e3ef14b21b090fa095aa90f93d82e
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-02-17 11:19:32 +0530

    navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-
-  
+    const isMobile = window.innerWidth <= 768;  
 
     const NavStyle = {
-        backgroundColor: "#2d3436"
+        backgroundColor: "#2d3436",
+        paddingRight: isMobile ? "0" : "10rem",
+        paddingLeft: isMobile ? "0" : "10rem",
     }
 
     const NavItem = {
@@ -20,23 +21,23 @@ const NavBar = () => {
         <div >
             <nav style={NavStyle} className="navbar navbar-expand-lg navbar-dark">
                 <div className="container-fluid ">
-                    <Link className="navbar-brand pt-0" style={{color: "rgb(58, 218, 255)", fontWeight: "700", fontSize: "1.8rem"}} to="/">NewsMonkey</Link>
+                    <Link className="navbar-brand pt-0" style={{color: "rgb(255, 255, 255)", fontWeight: "700", fontSize: "1.5rem"}} to="/">NewsWave</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul className="navbar-nav ">
                         <li style={NavItem} className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
-                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/about">About</Link> </li>
+                        {/* <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/about">About</Link> </li> */}
+                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link> </li>
                         <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/business">Business</Link> </li>
-                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link> </li>
-                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/general">General</Link> </li>
                         <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/health">Health</Link> </li>
                         <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/science">Science</Link> </li>
                         <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/sports">Sports</Link> </li>
-                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link> </li>
+                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/general">General</Link> </li>
+                        <li style={NavItem} className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link> </li>
                        
                     </ul>
                     </div>
